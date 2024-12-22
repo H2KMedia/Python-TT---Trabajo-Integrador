70315e29860341ae1a0e8287a6b93e9e2daa48fc
Esteban L <121070232+H2KMedia@users.noreply.github.com>
2024-12-22 14:28:52 -0300

    cambio de rubro para el tp, y actualizaciones varias sobre el nuevo proyecto

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def inicializar_bd():
 def mostrar_menu():
     #os.system('cls' if os.name == 'nt' else 'clear')
     print(150*"-")
-    print("|                                                       MENÚ DE OPCIONES PRODUCCION TEXTIL                                                        |")
+    print("|                                                         MENÚ DE OPCIONES PRODUCCIÓN TEXTIL                                                         |")
     print(150*"-")
     print("|                                                                                                                                                    |")
-    print("|                                                             1 - REGISTRAR ARTICULOS                                                                    |")
-    print("|                                                             2 - ACTUALIZAR ARTÍCULO                                                                   |")
-    print("|                                                             3 - ELIMINAR ARTÍCULO                                                                     |")
-    print("|                                                             4 - BUSCAR ARTICULO                                                                       |")
-    print("|                                                             5 - LISTADOS                                                                      |")
+    print("|                                                             1 - REGISTRAR ARTICULOS                                                                |")
+    print("|                                                             2 - ACTUALIZAR ARTÍCULO                                                                |")
+    print("|                                                             3 - ELIMINAR ARTÍCULO                                                                  |")
+    print("|                                                             4 - BUSCAR ARTICULO                                                                    |")
+    print("|                                                             5 - LISTADOS                                                                           |")
     print("|                                                             0 - SALIR DE SISTEMA                                                                   |")
     print("|                                                                                                                                                    |")
     print(150*"-")
@@ -38,9 +38,13 @@ def mostrar_menu():
 def agregar_registro():
     #os.system('cls' if os.name == 'nt' else 'clear')
     while True:
-        articulo = input("INGRESE EL ARTÏCULO: ")
-        descripcion = input("INGRESE UNA DESCRIPCIÖN DEL ARTICULO: ")
-        rubro = input("INGRESE EL RUBRO: ")
+        print(150*"-")
+        print("|                                                            SUBMENÚ CARGA DE ARTÍCULOS                                                              |")
+        print(150*"-") 
+        print()
+        articulo = input("INGRESE EL ARTÏCULO: ").upper()
+        descripcion = input("INGRESE UNA DESCRIPCIÖN DEL ARTICULO: ").upper()
+        rubro = input("INGRESE EL RUBRO: ").upper()
         stock = int(input("INGRESE EL STOCK: "))
         precio = float(input("INGRESE EL PRECIO: "))
 
@@ -63,23 +67,32 @@ def agregar_registro():
 # Función para modificar un registro
 def modificar_registro():
     #os.system('cls' if os.name == 'nt' else 'clear')
-    print("OPCIONES DE BÚSQUEDA: ")
-    print(" 1. ID")
-    print(" 2. ARTÍCULO ")
-    print(" 3. RUBRO")
+    print(150*"-")  
+    print("|                                                           SUBMENÚ MODIFICACIÓN DE RTÍCULOS                                                         |")
+    print(150*"-") 
+    print("|                                                                                                                                                    |")
+    print("|                                                             1 - BÚSQUEDA POR ID                                                                    |")
+    print("|                                                             2 - BÚSQUEDA POR ARTÍCULO                                                              |")
+    print("|                                                             3 - BUSQUEDA POR RUBRO                                                                 |")
+    print("|                                                                                                                                                    |")
+    print("|                                                             9 - VOLVER AL MENÚ ANTERIOR                                                            |")
+    print("|                                                                                                                                                    |")
+    print(150*"-")  
+    print()
+    
     opcion = int(input("SELECCIONE UNA OPCIÓN: "))
-
-    if opcion == '1':
-        referencia = input("INGRESE EL ID: ")
-        campo_busqueda = "id"
-    elif opcion == '2':
+    if opcion == 1:
+            referencia = input("INGRESE EL ID: ")
+            campo_busqueda = "id"
+    elif opcion == 2:
         referencia = input("INRESE EL ARTÍCULO: ")
         campo_busqueda = "articulo"
-    elif opcion == '3':
+    elif opcion == 3:
         referencia = input("INGRESE EL RUBRO: ")
         campo_busqueda = "rubro"
     else:
         print("OPCIÓN NO VÁLIDA, INTENTE NUEVAMENTE")
+        input("PRESIONE CUALQUIER ECLA PARA CONTINUAR...")
         return
 
     conn = sqlite3.connect('stock.db')
@@ -88,22 +101,18 @@ def modificar_registro():
         SELECT * FROM registros WHERE {campo_busqueda} = ?
     ''', (referencia,))
     registro = cursor.fetchone()
-
     if registro:
         id_registro = registro[0]
         print(f"ID: {registro[0]}, ARTÍCULO: {registro[1]}, DESCRIPCIÓN: {registro[2]}, RUBRO: {registro[3]}, STOCK: {registro[4]}, PRECIO: {registro[5]}")
-        
         nuevo_articulo = input("INGRESE EL NUEVO ARTÍCULO (DEJAR EN BLANCO PARA NO MODIFICAR): ").upper() or registro[1]
         nuevo_descripcion = input("INGRESE LA NUEVA DESCRIPCIÓN DEL ART. (DEJAR EN BLANCO PARA NO MODIFICAR): ").upper() or registro[2]
-        nuevo_rubro = input("INGRESE EL NUEVO RUBROL (DEJAR EN BLANCO PARA NO MODIFICAR): ").upper() or registro[3]
+        nuevo_rubro = input("INGRESE EL NUEVO RUBRO (DEJAR EN BLANCO PARA NO MODIFICAR): ").upper() or registro[3]
         nuevo_stock = input("INGRESE EL NUEVO STOCK (DEJAR EN BLANCO PARA NO MODIFICAR): ")
         nuevo_stock = int(nuevo_stock) if nuevo_stock else registro[4]
         nuevo_precio = input("INGRESE EL NUEVO PRECIO (DEJAR EN BLANCO PARA NO MODIFICAR: ")
         nuevo_precio = float(nuevo_precio) if nuevo_precio else registro[5]
-
         print(f"SE MODIFICARÁN LOS DATOS: ARTÍCULO: {nuevo_articulo}, DESCRIPCIÓN: {nuevo_descripcion}, RUBRO: {nuevo_rubro}, STOCK: {nuevo_stock}, PRECIO: {nuevo_precio}")
         confirmar = input("¿DESEA GUARDAR LOS CAMBIOS? (S/N): ")
-
         if confirmar.upper() == 'S':
             cursor.execute('''
                 UPDATE registros SET articulo = ?, descripcion = ?, rubro = ?, stock = ?, precio = ?
@@ -112,13 +121,20 @@ def modificar_registro():
             conn.commit()
             print("REGISTRO MODIFICADO.")
     else:
-        print("REGISTRO NO ENCONTRADO.")
-    
+        print()
+        print("<<<  REGISTRO NO ENCONTRADO >>>")
+        print()
+        input("PRESIONE CUALQUIER ECLA PARA CONTINUAR...")
+        os.system('cls' if os.name == 'nt' else 'clear')
     conn.close()
 
 # Función para eliminar un registro
 def eliminar_registro():
     #os.system('cls' if os.name == 'nt' else 'clear')
+    print(150*"-")  
+    print("|                                                           SUBMENÚ ELIMIRAR REGISTROS                                                               |")
+    print(150*"-") 
+    print("|                                                                                                                                                    |")
     id_eliminar = input("INGRESE EL ID DEL REGISTRO A ELIMINAR: ")
 
     conn = sqlite3.connect('stock.db')
@@ -129,7 +145,10 @@ def eliminar_registro():
     registro = cursor.fetchone()
 
     if registro:
-        print(f"ID: {registro[0]}, ARTÍCULO: {registro[1]}, DESCRIPCIÓN: {registro[2]}, RUBRO: {registro[3]}, STOCK: {registro[4]}, PRECIO: {registro[5]}")
+        print(150*"-") 
+        print(f"ID: {registro[0]}, ARTÍCULO: {registro[1]}, DESCRIPCIÓN: {registro[2]}, RUBRO: {registro[3]}, STOCK: {registro[4]}, PRECIO: $ {registro[5]}")
+        print(150*"-")
+        print()
         confirmar = input("¿DESEA ELIMINAR ESTE REGISTRO? (S/N): ")
 
         if confirmar.upper() == 'S':
@@ -144,20 +163,67 @@ def eliminar_registro():
         print("REGISTRO NO ENCONTRADO")
     
     conn.close()
+    
+def buscar_registros():
+    #os.system('cls' if os.name == 'nt' else 'clear')
+    print(150*"-")  
+    print("|                                                           SUBMENÚ BÚSQUEDA DE ARTÍCULOS                                                            |")
+    print(150*"-") 
+    print("|                                                                                                                                                    |")
+    print("|                                                             1 - BÚSQUEDA POR ID                                                                    |")
+    print("|                                                             2 - BÚSQUEDA POR ARTÍCULO                                                              |")
+    print("|                                                             3 - BUSQUEDA POR RUBRO                                                                 |")
+    print("|                                                                                                                                                    |")
+    print("|                                                             9 - VOLVER AL MENÚ ANTERIOR                                                            |")
+    print("|                                                                                                                                                    |")
+    print(150*"-")  
+    print()
+    
+    opcion = int(input("SELECCIONE UNA OPCIÓN: "))
+    if opcion == 1:
+            referencia = input("INGRESE EL ID: ")
+            campo_busqueda = "id"
+    elif opcion == 2:
+        referencia = input("INRESE EL ARTÍCULO: ")
+        campo_busqueda = "articulo"
+    elif opcion == 3:
+        referencia = input("INGRESE EL RUBRO: ")
+        campo_busqueda = "rubro"
+    else:
+        print("OPCIÓN NO VÁLIDA, INTENTE NUEVAMENTE")
+        input("PRESIONE CUALQUIER ECLA PARA CONTINUAR...")
+        return
 
+    conn = sqlite3.connect('stock.db')
+    cursor = conn.cursor()
+    cursor.execute(f'''
+        SELECT * FROM registros WHERE {campo_busqueda} = ?
+    ''', (referencia,))
+    registro = cursor.fetchone()
+    if registro:
+        id_registro = registro[0]
+        print(f"ID: {registro[0]}, ARTÍCULO: {registro[1]}, DESCRIPCIÓN: {registro[2]}, RUBRO: {registro[3]}, STOCK: {registro[4]}, PRECIO: $ {registro[5]}")
+    
 # Función para listar registros
 def listar_registros():
     #os.system('cls' if os.name == 'nt' else 'clear')
-    print("OPCIONES DE LISTADO: ")
-    print(" 1. TODOS LOS REGISTROS ")
-    print(" 2. REPORTE DE STOCK MÍNIMO")
+    print(150*"-")  
+    print("|                                                            SUBMENÚ LISTADO DE ARTÍCULOS                                                            |")
+    print(150*"-") 
+    print("|                                                                                                                                                    |")
+    print("|                                                             1 - TODOS LOS REGISTROS                                                                |")
+    print("|                                                             2 - REPORTE DE STOCK MÍNIMO                                                            |")
+    print("|                                                                                                                                                    |")
+    print("|                                                             9 - VOLVER AL MENÚ ANTERIOR                                                            |")
+    print("|                                                                                                                                                    |")
+    print(150*"-")  
+    print()
     opcion = int(input("SELECCIONE UNA OPCIÓN: "))
 
     conn = sqlite3.connect('stock.db')
     cursor = conn.cursor()
     
     if opcion == 1:
-        #  os.system('cls' if os.name == 'nt' else 'clear')
         cursor.execute('''
             SELECT * FROM registros ORDER BY id
         ''')
@@ -167,6 +233,9 @@ def listar_registros():
         cursor.execute('''
             SELECT * FROM registros WHERE stock <= ? ORDER BY id
         ''', (stock_limite,))
+    elif opcion == 3:
+        conn.close
+        menu()
     else:
         print("OPCIÓN NO VÁLIDA.")
         input("PRESIONE CUALQUIER TECLA PARA CONTINUAR...")
@@ -174,29 +243,38 @@ def listar_registros():
         conn.close()
         return
     
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(150*"-")
     registros = cursor.fetchall()
     for registro in registros:
-        print(f"ID: {registro[0]}, ARTÍCULO: {registro[1]}, DESCRIPCIÓN: {registro[2]}, RUBRO: {registro[3]}, STOCK: {registro[4]}, PRECIO: {registro[5]}")
+        print(f"| ID: {registro[0]}, | ARTÍCULO: {registro[1]}, | DESCRIPCIÓN: {registro[2]}, | RUBRO: {registro[3]}, | STOCK: {registro[4]}, | PRECIO: $ {registro[5]} |")
     conn.close()
+    print(150*"-")
+    print()
+    input("PRESIONE CUALQUIER TECLA PARA CONTINUAR...")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # Función principal del menú
 def menu():
     while True:
         mostrar_menu()
-        opcion = input("SELECCIONE UNA OPCIÓN: ")
-        if opcion == '1':
+        opcion = int(input("SELECCIONE UNA OPCIÓN: "))
+        if opcion == 1:
             os.system('cls' if os.name == 'nt' else 'clear')
             agregar_registro()
-        elif opcion == '2':
+        elif opcion == 2:
             os.system('cls' if os.name == 'nt' else 'clear')
             modificar_registro()
-        elif opcion == '3':
+        elif opcion == 3:
             os.system('cls' if os.name == 'nt' else 'clear')
             eliminar_registro()
-        elif opcion == '4':
+        elif opcion == 4:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            buscar_registros()
+        elif opcion == 5:
             os.system('cls' if os.name == 'nt' else 'clear')
             listar_registros()
-        elif opcion == '0':
+        elif opcion == 0:
             os.system('cls' if os.name == 'nt' else 'clear')
             print("...::: SALIENDO DEL PROGRAMA :::...")
             print()
